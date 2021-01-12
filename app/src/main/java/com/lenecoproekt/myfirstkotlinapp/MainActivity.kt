9f47a4e09cd9dc355b5fa3b82531b15afc21d0b6
rumigor <rumigor@gmail.com>
2021-01-12 10:20:12 +0300

    First program

--- app/src/main/java/com/lenecoproekt/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/lenecoproekt/myfirstkotlinapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.lenecoproekt.myfirstkotlinapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            (it as Button).text = "I am pushed"
+            text.text = "Button was pushed!"
+        }
     }
 }
